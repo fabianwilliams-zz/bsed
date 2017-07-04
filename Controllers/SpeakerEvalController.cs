5042bdc48a7dfbec4943f19c942107123c1ccc73
fabianwilliams <fabian@adotob.com>
2017-07-04 18:11:34 -0400

    Everything works All CRUD methods and All Containers Talking to eachother

--- Controllers/SpeakerEvalController.cs
+++ Controllers/SpeakerEvalController.cs
@@ -18,12 +18,72 @@ namespace app.Controllers
 		    _repository = repository;
 		}
 
-		// GET api/values
+		// GET api/speakereval
 		[HttpGet]
 		public IActionResult Get()
 		{
 			return Ok(_repository.GetAll().ToList());
 		}
 
+		// GET api/speakereval/3
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(int id)
+		{
+			var entity = await _repository.FindAsync(id);
+			if (entity == null)
+				return NotFound();
+
+			return Ok(entity);
+		}
+
+		// POST api/speakereval
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody]SpeakerEval model)
+		{
+			if (model == null)
+				return BadRequest("Please provide a Session Evaluation.");
+			if (string.IsNullOrEmpty(model.SessionName))
+				return BadRequest("An session name is required.");
+
+			var entity = await _repository.AddAsync(model);
+
+			return CreatedAtRoute(nameof(Get), entity.Id, entity);
+		}
+
+		// PUT api/speakereval/3
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Put(int id, [FromBody]SpeakerEval model)
+		{
+			if (model == null || model.Id != id)
+			{
+				return BadRequest();
+			}
+
+			var entity = await _repository.FindAsync(id);
+			if (entity == null)
+			{
+				return NotFound();
+			}
+
+			await _repository.UpdateAsync(model);
+			return new NoContentResult();
+		}
+
+		// DELETE api/speakereval/3
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			try
+			{
+				await _repository.RemoveAsync(id);
+			}
+			catch (InvalidOperationException e)
+			{
+				Console.WriteLine(e);
+				return BadRequest("No SpeakerSession found with id " + id);
+			}
+
+			return new NoContentResult();
+		}
 	}
 }
